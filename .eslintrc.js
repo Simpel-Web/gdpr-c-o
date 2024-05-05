10c9437caed94ba76aaca6212623489fdfac20b0
Stefan Herpich <s10h@posteo.de>
2024-05-05 22:54:01 +0200

    chore: fix linting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,15 @@ module.exports = {
         ecmaVersion: 2020,
         sourceType: 'module',
     },
-    extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'plugin:react/recommended',
+    ],
     rules: {},
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
 };
